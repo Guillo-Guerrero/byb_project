9ad52e15c1af9db7337456b9482814d7ae68b5ea
Guillermo Guerrero Vasquez <gagvasquez@gmail.com>
2024-04-07 20:26:42 +0100

    added hello_world.py to repository

---
+++ hello_world.py
@@ -0,0 +1,24 @@
+# Pseudocode:
+# 1. Prompt the user to enter their name using the input() function.
+# 2. Store the user's input in a variable called 'name'.
+# 3. Print out the value of 'name' to display the user's name.
+# 4. Prompt the user to enter their age using the input() function.
+# 5. Store the user's input in a variable called 'age'.
+# 6. Print out the value of 'age' to display the user's age.
+# 7. Print the string "Hello World!" on a new line.
+
+# Python code:
+# Prompt the user to enter their name
+name = input("Please enter your name: ")
+
+# Print out the user's name
+print("Your name is:", name)
+
+# Prompt the user to enter their age
+age = input("Please enter your age: ")
+
+# Print out the user's age
+print("Your age is:", age)
+
+# Print "Hello World!" on a new line
+print("Hello World!")
